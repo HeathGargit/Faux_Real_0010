32de3eaa333f4b7b7d87a5b10f72cee4a8da1563
Heath Gargit <gargit@gargit.net>
2019-01-05 15:43:22 +1100

    Added player character with movement and mousem look

--- Assets/_Scripts/Managers/GameController.cs
+++ Assets/_Scripts/Managers/GameController.cs
@@ -21,10 +21,18 @@ public class GameController : MonoBehaviour {
     //References to UI
     public UI_Controller m_UI;
 
+    //Refernce to the player for pausing and unpausing
+    public GameObject m_Player;
+
     // Use this for initialization
     void Start()
     {
+        //set the game state to iitialising
         m_GameState = GameState.Init;
+
+        //hide and lock the mouse
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -60,6 +68,16 @@ public class GameController : MonoBehaviour {
 
         //Show the pause menu
         m_UI.ShowPauseMenu();
+        //Show the cursor
+        Cursor.visible = true;
+        //Unlock the mouse
+        Cursor.lockState = CursorLockMode.None;
+
+        //set the time scale to stop
+        Time.timeScale = 0.0f;
+
+        //stop the players mouse look movement - player movement is handled by timescale and doesn't need to be stopped manually.
+        m_Player.GetComponent<MouseLook>().Pause();
     }
 
     private void UnPauseGame()
@@ -69,5 +87,15 @@ public class GameController : MonoBehaviour {
 
         //Hide the pause menu
         m_UI.HidePauseMenu();
+        //hide the cursor
+        Cursor.visible = false;
+        //Lock the mouse
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //set the time scale to move again
+        Time.timeScale = 1.0f;
+
+        //start the mouselook again
+        m_Player.GetComponent<MouseLook>().UnPause();
     }
 }
---
+++ Assets/_Scripts/Player/MouseLook.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    //variables to deal with mouse movement/sensetivity
+    public float m_MouseSensitivity = 15F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+
+    private bool isPaused = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().freezeRotation = true;
+
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (!isPaused)
+        {
+            //Rotate the x axis
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * m_MouseSensitivity;
+
+            //rotate the y, with a maximum rotation upwards so you dont do a backflip or front flip
+            rotationY += Input.GetAxis("Mouse Y") * m_MouseSensitivity;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            //change the rotation of the camera transform
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+    }
+
+    public void UnPause()
+    {
+        isPaused = false;
+    }
+}
